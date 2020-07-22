2c0703fc42aeabaff577f05bdd04e0b7262b3ca3
ahaikin <ahaikin@entrustpermal.com>
2020-07-22 01:11:15 -0400

    Add 'AutoGroupColumnDefinition'

---
+++ src/BlazorAgGrid/AutoGroupColumnDefinition.cs
@@ -0,0 +1,15 @@
+﻿namespace BlazorAgGrid
+{
+    public class AutoGroupColumnDefinition
+    {
+        public string HeaderName { get; set; }
+        public int Width { get; set; }
+        public int MinWidth { get; set; }
+        public CellRendererParams CellRendererParams { get; set; }
+    }
+
+    public class CellRendererParams
+    {
+        public bool SuppressCount { get; set; }
+    }
+}
